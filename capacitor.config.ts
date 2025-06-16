c675a870e8eb8c3222cf9fa7ba66b5f44df9d6fa
Maer costa <39601135+Aramunii@users.noreply.github.com>
2025-06-16 16:17:16 -0300

    adição do capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pokedex.app',
+  appName: 'pokedex',
+  webDir: 'www'
+};
+
+export default config;
